6f5fd8c16f328084052c7d5f0f76c5b5aa5ee484
신윤식 <joonshin79@gmail.com>
2020-12-08 10:02:47 +0900

    by Dates 수정

--- src/components/Module/Portfolio/Cryptocurrency/Left/Portfolio/byDates/byDates.vue
+++ src/components/Module/Portfolio/Cryptocurrency/Left/Portfolio/byDates/byDates.vue
@@ -2,30 +2,54 @@
   <table>
     <thead>
       <tr>
-        <th>Date</th>
-        <th><button type="button" class="active">All</button></th>
-        <th>Coin</th>
-        <th>Exchange</th>
-        <th>Price</th>
-        <th>Amount</th>
+        <th>
+          <span>Date</span>
+        </th>
+        <th>
+          <button type="button" class="active">All</button>
+        </th>
+        <th>
+          <p>
+            <span>Coin</span>
+            <span>Exchange</span>
+            <span>Price</span>
+            <span>Amount</span>
+          </p>
+        </th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>2020-07-20</td>
-        <td class="rise">Sell</td>
-        <td>GUSD</td>
-        <td>Upbit</td>
-        <td><em>₫</em>123,456,78<span>K</span></td>
-        <td>123,456,78<span>K</span></td>
+        <td>
+          <span>2020-07-20</span>
+        </td>
+        <td>
+          <span class="rise">Sell</span>
+        </td>
+        <td>
+          <p>
+            <span>GUSD</span>
+            <span>Upbit</span>
+            <span><em>₫</em>123,456,78<abbr>K</abbr></span>
+            <span>123,456,78<abbr>K</abbr></span>
+          </p>
+        </td>
       </tr>
       <tr>
-        <td>2020-07-20</td>
-        <td class="fall">Buy</td>
-        <td>GUSD</td>
-        <td>Upbit</td>
-        <td><em>₫</em>123,456,78<span>K</span></td>
-        <td>123,456,78<span>K</span></td>
+        <td>
+          <span>2020-07-20</span>
+        </td>
+        <td>
+          <span class="fall">Buy</span>
+        </td>
+        <td>
+          <p>
+            <span>GUSD</span>
+            <span>Upbit</span>
+            <span><em>₫</em>123,456,78<abbr>K</abbr></span>
+            <span>123,456,78<abbr>K</abbr></span>
+          </p>
+        </td>
       </tr>
     </tbody>
   </table>
--- src/components/Module/Portfolio/Cryptocurrency/Left/TotalValue/TotalValue.vue
+++ src/components/Module/Portfolio/Cryptocurrency/Left/TotalValue/TotalValue.vue
@@ -1,15 +1,84 @@
 <template>
-  <div class="Portfolio">
-    Portfolio
-  </div>
+    <div class="TotalValue">
+        <div>
+            <h2>Total Value</h2>
+            <p>
+                <em>₫</em>
+                <strong class="rise">175,580,000,000</strong>
+                <span>₿0.00000000.72</span>
+            </p>
+        </div>
+        <article :class="{'on': isMore}">
+            <button type="button" :class="{'on': isMore}" @click="btnMore">More</button>
+            <dl>
+                <dt>Buy Value</dt>
+                <dd>
+                    <em>₫</em>
+                    <strong>123,456,789</strong>
+                    <span>K</span>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Profit & Loss</dt>
+                <dd>
+                    <em>₫</em>
+                    <strong class="fall">123,456,789</strong>
+                    <span>K</span>
+                </dd>
+            </dl>
+            <dl>
+                <dt>%Profit </dt>
+                <dd>
+                    <strong class="fall">-10000<em>%</em></strong>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Buy Value</dt>
+                <dd>
+                    <em>₫</em>
+                    <strong>123,456,789</strong>
+                    <span>K</span>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Profit & Loss</dt>
+                <dd>
+                    <em>₫</em>
+                    <strong class="fall">123,456,789</strong>
+                    <span>K</span>
+                </dd>
+            </dl>
+            <dl>
+                <dt>%Profit </dt>
+                <dd>
+                    <strong class="fall">-10000<em>%</em></strong>
+                </dd>
+            </dl>
+        </article>
+    </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
+    data () {
+		return {
+            isMore: false
+        }
+    },
+    methods: {
+		btnMore: function(){
+            if(this.isMore == false){
+                this.isMore = true;
+            } else{
+                this.isMore = false;
+            }
+        },
+
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  @import "TotalValue.scss";
+    @import "TotalValue.scss";
 </style>
