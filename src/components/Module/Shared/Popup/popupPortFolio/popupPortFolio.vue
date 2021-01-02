aa2ef441fcebd55cf68a5228d9296753e22510df
신윤식 <joonshin79@gmail.com>
2021-01-02 10:36:58 +0900

    포트폴리오 등록팝업 내용추가

--- src/components/Module/Shared/Popup/popupPortFolio/popupPortFolio.vue
+++ src/components/Module/Shared/Popup/popupPortFolio/popupPortFolio.vue
@@ -1,11 +1,15 @@
 <template>
 <div class="layerPopup">
+	<button type="button" v-if="$windowWidth < 640">
+		<span>Back</span>
+		<i class="fas fa-arrow-left"/>
+		</button>
 	<p class="tit">Add Buy</p>
 	<div class="tabs">
-		<button type="button" class="active">Coin Buy</button>
-		<button type="button">Fiat Deposit</button>
+		<button type="button" :class="{'active': tabs.isCoin}" @click="btnCoin()">Coin Buy</button>
+		<button type="button" :class="{'active': tabs.isFiat}" @click="btnFiat()">Fiat Deposit</button>
 	</div>
-	<article>
+	<article v-if="tabs.isCoin">
 		<dl>
 			<dt>Coin</dt>
 			<dd class="full">
@@ -61,6 +65,42 @@
 			</dd>
 		</dl>
 	</article>
+
+	<article v-if="tabs.isFiat">
+		<dl>
+			<dt>Type</dt>
+			<dd class="full">
+				<input type="text">
+				<button type="button">
+					<i class="fas fa-search"/>
+					<span>Search</span>
+				</button>
+			</dd>
+			<dt>When</dt>
+			<dd class="full"><input type="date"></dd>
+			<dt>Where</dt>
+			<dd class="full">
+				<input type="text">
+				<button type="button">
+					<i class="fas fa-search"/>
+					<span>Search</span>
+				</button>
+			</dd>
+			<dt>How much</dt>
+			<dd class="full">
+				<input type="text" placeholder="1,000">
+				<em>KRW</em>
+			</dd>
+		</dl>
+		<dl>
+			<dt>Total Buy Value</dt>
+			<dd>
+				<em>$</em>
+				<span>2,399,000</span>
+			</dd>
+		</dl>
+	</article>
+
 	<div class="btnSet">
 		<button type="button">Cancel</button>
 		<button type="button">Buy</button>
@@ -70,6 +110,25 @@
 
 <script>
 export default {
+	data () {
+        return {
+			tabs:{
+				isCoin: true,
+				isFiat: false,
+			},
+
+        }
+    },
+	methods: {
+		btnCoin: function(){
+			this.tabs.isCoin = true;
+			this.tabs.isFiat = false;
+		},
+		btnFiat: function(){
+			this.tabs.isCoin = false;
+			this.tabs.isFiat = true;
+		},
+	}
 }
 </script>
 
