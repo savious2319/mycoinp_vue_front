190fbaae12921aded348bfd71eb87b65de477120
신윤식 <joonshin79@gmail.com>
2021-02-04 13:24:07 +0900

    plus 메뉴 추가

--- src/components/LayOut/Navigation/Navigation.vue
+++ src/components/LayOut/Navigation/Navigation.vue
@@ -1,38 +1,53 @@
 <template>
 	<div class="navigation">
 		<div>
-			<router-link class-active="active" to="/portfolio/legaltender">
+			<router-link to="/portfolio/legaltender">
 				<span v-if="$windowWidth > 1240"><i class="fas fa-user-friends"/></span>
 				<span>Portfolio</span>
 				<span>[25]</span>
 			</router-link>
-			<router-link class-active="active" to="/allcoin">
+			<router-link to="/allcoin">
 				<span v-if="$windowWidth > 1240"><i class="fas fa-coins"/></span>
 				<span>All Coin</span>
 				<span>[3122]</span>
 			</router-link>
-			<router-link class-active="active" to="/exchanges">
+			<router-link to="/exchanges">
 				<span v-if="$windowWidth > 1240"><i class="fas fa-exchange-alt"/></span>
 				<span>Exchanges</span>
 				<span>[41]</span>
 			</router-link>
-			<router-link class-active="active" to="/defi">
+			<router-link to="/defi">
 				<span v-if="$windowWidth > 1240"><i class="fas fa-landmark"/></span>
 				<span>Defi</span>
 				<span>[41]</span>
 			</router-link>
-			<a href="#">
-				<span v-if="$windowWidth > 1240"><i class="fas fa-ellipsis-h"/></span>
-				<span>++Plus</span>
-				<i class="fas fa-angle-down"/>
+			<a href="#none" :class="{'router-link-exact-active': isPlus}" @click="isPlus = !isPlus">
+				<span v-if="$windowWidth < 640"><i class="fas fa-ellipsis-v"/></span>
+				<span v-if="$windowWidth > 640"><i class="fas fa-ellipsis-h"/></span>
+				<span v-if="$windowWidth > 1240">++Plus</span>
+				<span v-if="$windowWidth > 1240"><i class="fas fa-angle-down"/></span>
 			</a>
-			<ul>
-				<li><router-link class-active="active" to="/defi">Hi-Lo</router-link></li>
-				<li><router-link class-active="active" to="/defi">Abnormal Price</router-link></li>
-				<li><router-link class-active="active" to="/defi">World Map</router-link></li>
-				<li><router-link class-active="active" to="/defi">Expert</router-link></li>
-				<li><router-link class-active="active" to="/defi">Promotion</router-link></li>
-			</ul>
+			<div v-if="isPlus">
+				<router-link to="/Hi-Lo">
+					<i class="fas fa-exchange-alt"/>
+					<span>Hi-Lo</span>
+				</router-link>
+				<router-link to="/Abnormal-Price">
+					<i class="fas fa-asterisk"/>
+					<span>Abnormal Price</span>
+				</router-link>
+				<router-link to="/World-Map">
+					<i class="fas fa-map-marked-alt"/>
+					<span>World Map</span>
+				</router-link>
+				<router-link to="/Expert">
+					<i class="fas fa-user-tie"/>
+					<span>Expert</span>
+				</router-link>
+				<router-link to="/Promotion">
+					<span>Promotion</span>
+				</router-link>
+			</div>
 		</div>
 		<NavigationGasPrice v-if="$windowWidth > 640"/>
 	</div>
@@ -40,6 +55,12 @@
 
 <script>
 export default {
+	data () {
+        return {
+            isPlus: false,
+        }
+    },
+
 }
 </script>
 
